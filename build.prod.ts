32fb53e77ad20ced3355a42175961dbd18afb8b3
sbjovanovski <slobodanjovanovski@yahoo.com>
2025-07-26 12:53:46 +0800

    Fix build

--- build.prod.ts
+++ build.prod.ts
@@ -4,20 +4,22 @@ import postCssPlugin from 'esbuild-style-plugin'
 
 config()
 
-esbuild.buildSync({
-  entryPoints: ['./src/index.tsx'],
-  outdir: 'public',
-  bundle: true,
-  platform: 'node',
-  plugins: [
-    postCssPlugin({
-      postcss: {
-        plugins: [require('@tailwindcss/postcss'), require('autoprefixer')],
-      },
-    }),
-  ],
-  define: {
-    'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`,
-    'process.env.API_KEY': `"${process.env.API_KEY}"`,
-  },
-})
+;(async () => {
+  await esbuild.build({
+    entryPoints: ['./src/index.tsx'],
+    outdir: 'public',
+    bundle: true,
+    platform: 'node',
+    plugins: [
+      postCssPlugin({
+        postcss: {
+          plugins: [require('@tailwindcss/postcss'), require('autoprefixer')],
+        },
+      }),
+    ],
+    define: {
+      'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`,
+      'process.env.API_KEY': `"${process.env.API_KEY}"`,
+    },
+  })
+})()
